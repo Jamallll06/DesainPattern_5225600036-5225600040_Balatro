4c4a050858eb2badde6f5f1a246570d6d383f0b2
Divv-source <auliadiva2856@gmail.com>
2026-09-21 17:50:44 +0700

    TAKS 1 FIX

--- Balatro/Balatro/Task 1.cpp
+++ Balatro/Balatro/Task 1.cpp
@@ -47,3 +47,64 @@ public:
     }
 };
 
+class ShopSystem {
+public:
+    void PrintOffer(int currentMoney) {
+        std::cout << "[SHOP] offered: Bonus (+2) cost 2\n";
+        if (currentMoney >= 2) {
+            std::cout << "[SHOP] bought Bonus!\n";
+        }
+        else {
+            std::cout << "[SHOP] skipped\n";
+        }
+    }
+};
+
+class RunSession {
+private:
+    std::unique_ptr<IInputGenerator> inputGen;
+    std::unique_ptr<IScoringRule> scoringRule;
+    std::unique_ptr<IRewardRule> rewardRule;
+    ShopSystem shop;
+    int money = 0;
+    int round = 1;
+
+public:
+    RunSession(std::unique_ptr<IInputGenerator> ig,
+        std::unique_ptr<IScoringRule> sr,
+        std::unique_ptr<IRewardRule> rr)
+        : inputGen(std::move(ig)), scoringRule(std::move(sr)), rewardRule(std::move(rr)) {
+    }
+
+    void StartRun() {
+        std::cout << "=== RUN START ===\n";
+        while (round <= 3) {
+            std::cout << "Round " << round << "\n";
+
+            int input = inputGen->GenerateInput();
+            std::cout << "[PLAY] input generated: " << input << "\n";
+
+            int baseScore = scoringRule->ComputeBaseScore(input);
+            std::cout << "[SCORE] base score: " << baseScore << "\n";
+
+            int reward = rewardRule->ComputeReward(baseScore, round);
+            money += reward;
+            std::cout << "[REWARD] gain: " << reward << " | money: " << money << "\n";
+
+            shop.PrintOffer(money);
+
+            round++;
+        }
+        std::cout << "=== RUN END ===\nFinal money: " << money << "\n";
+    }
+};
+
+int main() {
+    RunSession game(
+        std::make_unique<RandomInputGenerator>(),
+        std::make_unique<BaseScoringRule>(),
+        std::make_unique<DynamicRewardRule>()
+    );
+    game.StartRun();
+    return 0;
+}
